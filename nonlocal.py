807e972bf85498ee0e314365a7eb0eea977ac36a
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-06-09 00:16:11 +0800

    Create nonlocal.py

---
+++ nonlocal.py
@@ -0,0 +1,16 @@
+nonlocal关键字用来在函数或其他作用域中使用外层(非全局)变量。
+
+def work():
+    x = 0
+    def new_work():
+        nonlocal x
+        x=x+3
+        return x
+    return new_work
+         
+f=work()
+print(f())
+print(f())
+print(f())
+
+打印结果:3,6,9
