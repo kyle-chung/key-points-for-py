e99058489e166c6d73dcdb5a613913400405ba50
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-11-17 12:49:34 +0800

    Update collections.py

--- collections.py
+++ collections.py
@@ -70,3 +70,23 @@ defaultdict(<class 'int'>, {'apple': 2, 'banana': 0})
 ... 
 >>> d
 defaultdict(<class 'list'>, {'NC': ['Raleigh', 'Asheville'], 'VA': ['Richmond'], 'WA': ['Seattle']})
+
+
+
+OrderedDict
+Python字典中的键的顺序是任意的:它们不受添加的顺序的控制
+collections.OrderedDict类提供了保留他们添加顺序的字典对象
+
+>>> from collections import OrderedDict
+>>> o = OrderedDict()
+>>> o['key1'] = 'value1'
+>>> o['key2'] = 'value2'
+>>> o['key3'] = 'value3'
+>>> o
+OrderedDict([('key1', 'value1'), ('key2', 'value2'), ('key3', 'value3')])
+
+如果在已经存在的key上添加新的值，将会保留原来的key的位置，然后覆盖value值
+
+>>> o['key1'] = 'value5'
+>>> o
+OrderedDict([('key1', 'value5'), ('key2', 'value2'), ('key3', 'value3')])
