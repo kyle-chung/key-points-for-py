7eb3976eb8fdc23bc0d787b4ebaa247c2284ecce
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-11-17 13:17:44 +0800

    Update collections.py

--- collections.py
+++ collections.py
@@ -1,11 +1,51 @@
 这个模块实现了特定目标的容器，以提供Python标准内建容器 dict、list、set、tuple 的替代选择
 
+deque：类似列表容器，实现了在两端快速添加(append)和弹出(pop)
 Counter：字典的子类，提供了可哈希对象的计数功能
 defaultdict：字典的子类，提供了一个工厂函数，为字典查询提供了默认值
 OrderedDict：字典的子类，保留了他们被添加的顺序
 namedtuple：创建命名元组子类的工厂函数
-deque：类似列表容器，实现了在两端快速添加(append)和弹出(pop)
 ChainMap：类似字典的容器类，将多个映射集合到一个视图里面
+
+
+
+deque
+collections.deque返回一个新的双向队列对象，从左到右初始化(用方法 append()) ，从 iterable （迭代对象) 数据创建
+如果 iterable 没有指定，新队列为空
+collections.deque队列支持线程安全，对于从两端添加(append)或者弹出(pop)，复杂度O(1)
+虽然list对象也支持类似操作，但是这里优化了定长操作（pop(0)、insert(0,v)）的开销
+
+如果 maxlen 没有指定或者是 None ，deques 可以增长到任意长度
+否则，deque就限定到指定最大长度。一旦限定长度的deque满了，当新项加入时，同样数量的项就从另一端弹出。
+
+支持的方法：
+
+append(x)：添加x到右端
+appendleft(x)：添加x到左端
+clear()：清楚所有元素，长度变为0
+copy()：创建一份浅拷贝
+count(x)：计算队列中个数等于x的元素
+extend(iterable)：在队列右侧添加iterable中的元素
+extendleft(iterable)：在队列左侧添加iterable中的元素，注：在左侧添加时，iterable参数的顺序将会反过来添加
+index(x[,start[,stop]])：返回第 x 个元素（从 start 开始计算，在 stop 之前）。返回第一个匹配，如果没找到的话，升起 ValueError 。
+insert(i,x)：在位置 i 插入 x 。注：如果插入会导致一个限长deque超出长度 maxlen 的话，就升起一个 IndexError 。
+pop()：移除最右侧的元素
+popleft()：移除最左侧的元素
+remove(value)：移去找到的第一个 value。没有抛出ValueError
+reverse()：将deque逆序排列。返回 None 。
+maxlen：队列的最大长度，没有限定则为None。
+>>> from collections import deque
+>>> d = deque(maxlen=10)
+>>> d
+deque([], maxlen=10)
+>>> d.extend('python')
+>>> [i.upper() for i in d]
+['P', 'Y', 'T', 'H', 'O', 'N']
+>>> d.append('e')
+>>> d.appendleft('f')
+>>> d
+deque(['f', 'p', 'y', 't', 'h', 'o', 'n', 'e'], maxlen=10)
+
 
 
 Counter
